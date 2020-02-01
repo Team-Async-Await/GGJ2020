c75736df223b30f8b1eee645128a4fe91644aed6
Gabriel Paixao <gabriel.j.paixao@outlook.com>
2020-02-01 01:53:25 -0300

    My changes

---
+++ Assets/Scripts/Character/CharacterController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField]
+    private float _moveSpeed;
+    private Vector2 _moveInput;
+
+    public Sprite Idle;
+    public Sprite Gun;
+
+    public Rigidbody2D Body;
+    public SpriteRenderer Sprite;
+
+    public bool haveGun = false;
+
+    private void Start()
+    {
+        Body = GetComponent<Rigidbody2D>();
+        Sprite = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        _moveInput.x = Input.GetAxisRaw("Horizontal_P2");
+        _moveInput.y = Input.GetAxisRaw("Vertical_P2");
+
+        Body.velocity = _moveInput * _moveSpeed;
+
+        if (haveGun)
+        {
+            Sprite.sprite = Gun;
+        }
+        else
+        {
+            Sprite.sprite = Idle;
+        }
+
+        if (_moveInput.x > 0)
+            transform.rotation = Quaternion.AngleAxis(0, Vector3.forward);
+        else if (_moveInput.x < 0)
+            transform.rotation = Quaternion.AngleAxis(180, Vector3.forward);
+        else if (_moveInput.y > 0)
+            transform.rotation = Quaternion.AngleAxis(90, Vector3.forward);
+        else if (_moveInput.y < 0)
+            transform.rotation = Quaternion.AngleAxis(-90, Vector3.forward);
+    }
+}
---
+++ Assets/Scripts/Character/PlayerBullet.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+    public float Speed = 7.5f;
+    private Rigidbody2D _body;
+    private void Start()
+    {
+        _body = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        _body.velocity = transform.up * Speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject);
+    }
+}
